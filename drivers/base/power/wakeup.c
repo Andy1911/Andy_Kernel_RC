513ed8a89814e8fe7671b267c025b0ec771387aa
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-03 04:27:43 +0000

    wakeup: add toggle for bluedroid_timer wakelock
    Bluetooth might stop working properly with this wakeup source disabled, but it might be useful for users affected by this apparent bug.
    Bug-tracker: https://code.google.com/p/android/issues/detail?id=87778
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    
    	drivers/base/power/wakeup.c

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,6 +16,19 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
+<<<<<<< HEAD
+=======
+#include <linux/moduleparam.h>
+
+static bool enable_wlan_rx_wake_ws = true;
+module_param(enable_wlan_rx_wake_ws, bool, 0644);
+static bool enable_wlan_ctrl_wake_ws = true;
+module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
+static bool enable_wlan_wake_ws = true;
+module_param(enable_wlan_wake_ws, bool, 0644);
+static bool enable_bluedroid_timer_ws = true;
+module_param(enable_bluedroid_timer_ws, bool, 0644);
+>>>>>>> 6d4c0c7... wakeup: add toggle for bluedroid_timer wakelock
 
 #include "power.h"
 
@@ -443,6 +456,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	 * out of PM_SUSPEND_FREEZE state
 	 */
 	freeze_wake();
+
+	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
+		return;
 
 	ws->active = true;
 	ws->active_count++;
